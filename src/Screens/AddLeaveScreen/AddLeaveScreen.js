04739e5348dc5e22e43386180b6ce93a1673ba0f
ravindersharma83 <ravinder200198@gmail.com>
2023-09-01 15:12:25 +0530

    Creating Apply for leave screen ui using radio button and datetime picker

--- src/Screens/AddLeaveScreen/AddLeaveScreen.js
+++ src/Screens/AddLeaveScreen/AddLeaveScreen.js
@@ -7,6 +7,8 @@ import imagePath from '../../Constants/imagePath';
 import { moderateScale } from 'react-native-size-matters';
 import colors from '../../Styles/colors';
 import TextInputWithLabel from '../../Components/TextInputWithLabel';
+import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'react-native-simple-radio-button';
+import ButtonComp from '../../Components/ButtonComp';
 
 const AddLeaveScreen = ({navigation}) => {
   const [startDate, setStartDate] = useState(new Date());
@@ -14,40 +16,48 @@ const AddLeaveScreen = ({navigation}) => {
   const [startTime, setStartTime] = useState(new Date());
   const [endTime, setEndTime] = useState(new Date());
   const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(null);
+  const [show, setShow] = useState('none');
 
-  const onDateChange = (event, selectedDate, type) => {
-    setShow(null);
-    if (type === 'start') {
-      setStartDate(selectedDate);
-    } else {
-      setEndDate(selectedDate);
-    }
+  const [leaves, setLeaves] = useState(0);
+  const [startDay, setStartDay] = useState(0);
+  const [endDay, setEndDay] = useState(0);
+
+  const leave_props = [
+    { label: 'Planned', value: 'plan' },
+    { label: 'Emergency', value: 'emergency' },
+    { label: 'Short', value: 'short' },
+  ];
+  const day_props = [
+    { label: 'Full Day', value: 'full day' },
+    { label: 'Half Day', value: 'half day' },
+  ];
+
+  const onStartDateChange = (event, selectedDate) => {
+    const selectedStartDate = selectedDate;
+    setShow('none');
+    setStartDate(selectedStartDate);
   };
 
-  const onTimeChange = (event, selectedTime, type) => {
-    setShow(null);
-    if (type === 'start') {
-      setStartTime(selectedTime);
-    } else {
-      setEndTime(selectedTime);
-    }
+  const onEndDateChange = (event, selectedDate) => {
+    const selectedEndDate = selectedDate;
+    setShow('none');
+    setEndDate(selectedEndDate);
   };
 
-  const showPicker = (currentMode, pickerType) => {
+  const onStartTimeChange = (event, selectedTime) => {
+    const selectedStartTime = selectedTime;
+    setShow('none');
+    setStartTime(selectedStartTime);
+  };
+
+  const onEndTimeChange = (event, selectedTime) => {
+    const selectedEndTime = selectedTime;
+    setShow('none');
+    setEndTime(selectedEndTime);
+  };
+  const showMode = (currentMode,pickerType) => {
     setShow(pickerType);
     setMode(currentMode);
-  };
-
-  const renderDateTimePicker = (value, onChange) => {
-    return show === value && (
-      <DateTimePicker
-        testID="dateTimePicker"
-        value={value === 'start' ? startDate : endDate}
-        mode={mode}
-        onChange={(event, selectedDate) => onChange(event, selectedDate, value)}
-      />
-    );
   };
 
   return (
@@ -57,46 +67,173 @@ const AddLeaveScreen = ({navigation}) => {
         <TextInputWithLabel label={'Leave Reason'} placeholder={'Enter Leave Reason'} multiline={true}
         numberOfLines={2}/>
 
-        <TouchableOpacity onPress={() => showPicker('date', 'start')}>
-          <TextInputWithLabel
-            value={startDate.toDateString()}
-            label={'Start Date'}
-            placeholder={'Select Start Date'}
-            editable={false}
-          />
+      <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:10}}>
+        <TouchableOpacity onPress={()=>showMode('date','showStartDatePicker')}>
+          <TextInputWithLabel inputStyle={{width:moderateScale(150)}} value={startDate.toDateString()} label={'Start Date'} placeholder={'Select Start Date'} editable={false}/>
         </TouchableOpacity>
-        {renderDateTimePicker('start', onDateChange)}
+        {show==="showStartDatePicker" && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={startDate}
+            mode={mode}
+            // is24Hour={true}
+            onChange={onStartDateChange}
+          />
+        )}
+      {leave_props[leaves].label != 'Short' ? <>
+        <TouchableOpacity onPress={()=>showMode('date','showEndDatePicker')}>
+          <TextInputWithLabel inputStyle={{width:moderateScale(150)}} value={endDate.toDateString()} label={'End Date'} placeholder={'Select End Date'} editable={false}/>
+        </TouchableOpacity>
+        {show==="showEndDatePicker" && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={endDate}
+            mode={mode}
+            // is24Hour={true}
+            onChange={onEndDateChange}
+          />
+        )}
+        </> : '' 
+      }
+      </View>
 
-        <TouchableOpacity onPress={() => showPicker('date', 'end')}>
-          <TextInputWithLabel
-            value={endDate.toDateString()}
-            label={'End Date'}
-            placeholder={'Select End Date'}
-            editable={false}
-          />
-        </TouchableOpacity>
-        {renderDateTimePicker('end', onDateChange)}
+      {leave_props[leaves].label != 'Short' ? 
+            <>
+              <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:10}}>
+              <RadioForm formHorizontal={true} animation={true}>
+                {day_props.map((obj, i) => (
+                  <RadioButton labelHorizontal={true} key={i}>
+                    <RadioButtonInput
+                      obj={obj}
+                      index={i}
+                      isSelected={startDay === i}
+                      onPress={() => setStartDay(i)}
+                      borderWidth={1}
+                      buttonInnerColor={colors.themeColor}
+                      buttonOuterColor={startDay === i ? colors.themeColor : '#000'}
+                      buttonSize={9}
+                      buttonOuterSize={18}
+                      buttonStyle={{}}
+                      buttonWrapStyle={{ marginLeft: 10 }}
+                    />
+                    <RadioButtonLabel
+                      obj={obj}
+                      index={i}
+                      labelHorizontal={true}
+                      onPress={() => setStartDay(i)}
+                      labelStyle={{ fontSize:11, fontWeight: 'bold', color: '#000' }}
+                    />
+                  </RadioButton>
+                ))}
+              </RadioForm>
 
-        <TouchableOpacity onPress={() => showPicker('time', 'start')}>
-          <TextInputWithLabel
-            value={startTime.toTimeString()}
-            label={'Start Time'}
-            placeholder={'Select Start Time'}
-            editable={false}
-          />
-        </TouchableOpacity>
-        {renderDateTimePicker('start', onTimeChange)}
+              <RadioForm formHorizontal={true} animation={true}>
+                {day_props.map((obj, i) => (
+                  <RadioButton labelHorizontal={true} key={i}>
+                    <RadioButtonInput
+                      obj={obj}
+                      index={i}
+                      isSelected={endDay === i}
+                      onPress={() => setEndDay(i)}
+                      borderWidth={1}
+                      buttonInnerColor={colors.themeColor}
+                      buttonOuterColor={endDay === i ? colors.themeColor : '#000'}
+                      buttonSize={9}
+                      buttonOuterSize={18}
+                      buttonStyle={{}}
+                      buttonWrapStyle={{ marginLeft: 10 }}
+                    />
+                    <RadioButtonLabel
+                      obj={obj}
+                      index={i}
+                      labelHorizontal={true}
+                      onPress={() => setEndDay(i)}
+                      labelStyle={{ fontSize:11, fontWeight: 'bold', color: '#000' }}
+                    />
+                  </RadioButton>
+                ))}
+              </RadioForm>
+              </View>
+            </> 
+            : ''
+      }
 
-        <TouchableOpacity onPress={() => showPicker('time', 'end')}>
-          <TextInputWithLabel
-            value={endTime.toTimeString()}
-            label={'End Time'}
-            placeholder={'Select End Time'}
-            editable={false}
-          />
-        </TouchableOpacity>
-        {renderDateTimePicker('end', onTimeChange)}
-        
+      {
+        leave_props[leaves].label === 'Short' ? (
+          <React.Fragment>
+            <View style={{flexDirection:'row',justifyContent:'space-between',marginVertical:10}}>
+            <TouchableOpacity onPress={() => showMode('time', 'showStartTimePicker')}>
+              <TextInputWithLabel
+                value={startTime.toLocaleTimeString()}
+                label={'Start Time'}
+                placeholder={'Select Start Time'}
+                editable={false}
+                inputStyle={{width:moderateScale(150)}}
+              />
+            </TouchableOpacity>
+            {show === 'showStartTimePicker' && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={startTime}
+                mode={mode}
+                // is24Hour={true}
+                onChange={onStartTimeChange}
+              />
+            )}
+
+            <TouchableOpacity onPress={() => showMode('time', 'showEndTimePicker')}>
+              <TextInputWithLabel
+                value={endTime.toLocaleTimeString()}
+                label={'End Time'}
+                placeholder={'Select End Time'}
+                editable={false}
+                inputStyle={{width:moderateScale(150)}}
+              />
+            </TouchableOpacity>
+            {show === 'showEndTimePicker' && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={endTime}
+                mode={mode}
+                // is24Hour={true}
+                onChange={onEndTimeChange}
+              />
+            )}
+            </View>
+          </React.Fragment>
+        ) : null
+      }
+
+      <Text style={{marginVertical:moderateScale(8)}}>Leave Type</Text>
+      <RadioForm formHorizontal={true} animation={true}>
+        {leave_props.map((obj, i) => (
+          <RadioButton labelHorizontal={true} key={i}>
+            <RadioButtonInput
+              obj={obj}
+              index={i}
+              isSelected={leaves === i}
+              onPress={() => setLeaves(i)}
+              borderWidth={1}
+              buttonInnerColor={colors.themeColor}
+              buttonOuterColor={leaves === i ? colors.themeColor : '#000'}
+              buttonSize={10}
+              buttonOuterSize={20}
+              buttonStyle={{}}
+              buttonWrapStyle={{ marginLeft: 10 }}
+            />
+            <RadioButtonLabel
+              obj={obj}
+              index={i}
+              labelHorizontal={true}
+              onPress={() => setLeaves(i)}
+              labelStyle={{ fontWeight: 'bold', color: '#000' }}
+            />
+          </RadioButton>
+        ))}
+      </RadioForm>
+      </View>
+      <View style={{flex:1,margin:moderateScale(20),justifyContent:'flex-end'}}>
+        <ButtonComp btnText={'Apply Leave'}/>
       </View>
     </View>
   )
